7b35e151e72469714f3147ff5e9028c9e84042cb
vero002 <veronica.pepe00@gmail.com>
2026-02-24 13:33:11 +0100

    fix requirements

--- app/database.py
+++ app/database.py
@@ -5,4 +5,17 @@ from sqlalchemy.orm import sessionmaker
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+# 👇 QUESTA FUNZIONE TI MANCA
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
