823b6ae80a0752e927d5d1460d899e941b8d4e9c
vero002 <veronica.pepe00@gmail.com>
2026-02-25 11:40:31 +0100

    go

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
-from fastapi import FastAPI
-from app.database import engine
+from fastapi import FastAPI, Request, Form, Depends
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+
+from app.database import SessionLocal, engine
 from app import models
 from app.routers import ddt, veicoli, cantieri
 
 app = FastAPI()
+templates = Jinja2Templates(directory="app/templates")
 
 @app.on_event("startup")
 def startup():
@@ -15,7 +20,6 @@ app.include_router(ddt.router)
 app.include_router(veicoli.router)
 app.include_router(cantieri.router)
 
-
-@app.get("/")
-def home():
-    return {"status": "DDT Manager online"}
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
